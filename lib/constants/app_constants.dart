6b88dbb29fc0daa949669c948bc1fe2f1a1aecfd
Salah Haddara <Salahhaddara02@gmail.com>
2025-09-12 20:39:17 +0300

    Add app constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,19 @@
+class AppConstants {
+  static const String appName = 'Mini Ecommerce';
+  static const String appVersion = '1.0.0';
+
+  // Storage Keys
+  static const String tokenKey = 'auth_token';
+  static const String userKey = 'user_data';
+
+  static const double taxRate = 0.05;
+
+  static const int lowStockThreshold = 5;
+
+  static const int defaultPageSize = 20;
+
+  static const int minPasswordLength = 6;
+  static const int maxProductNameLength = 100;
+  static const double minProductPrice = 0.01;
+  static const int maxProductStock = 9999;
+}
