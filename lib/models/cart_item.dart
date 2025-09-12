56f7b105d6871a65217709eb30acac5a5ef200ba
Salah Haddara <Salahhaddara02@gmail.com>
2025-09-12 23:37:38 +0300

    Add cart item model

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,27 @@
+import 'product.dart';
+
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({
+    required this.product,
+    required this.quantity,
+  });
+
+  double get totalPrice => product.price * quantity;
+
+  String get formattedTotalPrice {
+    return '\$${totalPrice.toStringAsFixed(2)}';
+  }
+
+  CartItem copyWith({
+    Product? product,
+    int? quantity,
+  }) {
+    return CartItem(
+      product: product ?? this.product,
+      quantity: quantity ?? this.quantity,
+    );
+  }
+}
